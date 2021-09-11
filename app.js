a8a893b7e300fefee64e144c6598e9c9404c32d9
Mephysics <ichigoez0@gmail.com>
2021-09-12 03:25:39 +0700

    4.3.6 Release

--- app.js
+++ app.js
@@ -814,7 +814,7 @@ client.on('message', async message => {
     if (!client.commands.has(command)) return;
 
     try {
-        client.commands.get(command).execute(message, args);
+        client.commands.get(command).execute(client, message, args);
     } catch (error) {
         console.error(error);
         message.reply(process.env.DEFAULT_ERROR);
